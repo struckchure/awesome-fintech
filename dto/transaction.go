4949858ff30fae78ca7c0d38a63d4674ea95f239
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2024-05-06 15:23:46 +0100

    feat(transaction): implements queue system for transactions processing

--- dto/transaction.go
+++ dto/transaction.go
@@ -6,14 +6,13 @@ type ListTransactionDto struct {
 }
 
 type CreateTransactionDto struct {
-	AllowOverdraft *bool                   `json:"allowOverdraft"`
-	Source         string                  `json:"source"`
-	Destination    string                  `json:"destination"`
-	Reference      string                  `json:"reference"`
-	Amount         int64                   `json:"amount"`
-	Currency       string                  `json:"currency"`
-	Status         *string                 `json:"status,omitempty"`
-	Meta           *map[string]interface{} `json:"meta,omitempty"`
+	Source      string                 `json:"source"`
+	Destination string                 `json:"destination"`
+	Reference   string                 `json:"reference"`
+	Amount      int64                  `json:"amount"`
+	Currency    string                 `json:"currency"`
+	Status      string                 `json:"status,omitempty"`
+	Meta        map[string]interface{} `json:"meta,omitempty"`
 }
 
 type GetTransactionDto struct {
@@ -21,15 +20,14 @@ type GetTransactionDto struct {
 }
 
 type UpdateTransactionDto struct {
-	Id             string                  `json:"id"`
-	AllowOverdraft *bool                   `json:"allowOverdraft,omitempty"`
-	Source         *string                 `json:"source,omitempty"`
-	Destination    *string                 `json:"destination,omitempty"`
-	Reference      *string                 `json:"reference,omitempty"`
-	Amount         *int64                  `json:"amount,omitempty"`
-	Currency       *string                 `json:"currency,omitempty"`
-	Status         *string                 `json:"status,omitempty"`
-	Meta           *map[string]interface{} `json:"meta,omitempty"`
+	Id          string                  `json:"id"`
+	Source      *string                 `json:"source,omitempty"`
+	Destination *string                 `json:"destination,omitempty"`
+	Reference   *string                 `json:"reference,omitempty"`
+	Amount      *int64                  `json:"amount,omitempty"`
+	Currency    *string                 `json:"currency,omitempty"`
+	Status      *string                 `json:"status,omitempty"`
+	Meta        *map[string]interface{} `json:"meta,omitempty"`
 }
 
 type DeleteTransactionDto struct {
